9331ebdb10433e7221e4bc58c632c230615e5a5d
PauLOk777 <trocjuk2000@gmail.com>
2019-03-04 20:25:38 +0200

    Basic project structure

---
+++ lab1/lab1/Team.cpp
@@ -0,0 +1,22 @@
+#include "Team.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Team {
+private:
+	string name;
+	int wins;
+	int defeats;
+	int goals;
+	int misses;
+	int score;
+public:
+	Team(string info) {
+		//...
+	}
+	void parseInfo(string info) {
+
+	}
+};
