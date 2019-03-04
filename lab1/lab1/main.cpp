284b6b548676b647ce1f2b4924ae536be8b66cde
PauLOk777 <trocjuk2000@gmail.com>
2019-03-04 20:51:59 +0200

    Modified main.cpp, then create test.txt with input info

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -1,9 +1,26 @@
 #include "Team.h"
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+	ifstream fin;
+	fin.open("test.txt");
+	if (!fin.is_open()) {
+		cout << "Error" << endl;
+		return 1;
+	}
+	else {
+		cout << "File is opened" << endl;
+	}
+	string temp;
+	while (!fin.eof()) {
+		getline(fin, temp);
+		cout << temp << endl;
+	}
 
+	system("pause");
+	return 0;
 }
