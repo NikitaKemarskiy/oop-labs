1bb033824ecba8e4e755b8c5cff4e850668ba39c
NikitaKemarskiy <sashapobeditel1980@gmail.com>
2019-03-18 23:57:46 +0200

    In HashMap function hash() method was added and add() method was updated.

--- lab2/cpp/HashMap.cpp
+++ lab2/cpp/HashMap.cpp
@@ -19,22 +19,6 @@ HashMap::HashMap(int size) {
 	this->arr = new LinkedList[this->size];
 }
 
-void HashMap::add(string value) { // Добавление, хеш на основе самого значения
-	amount++;
-	//...
-	checkLoad();
-}
-
-void HashMap::add(string key, string value) { // Добавление, хеш на основе переданного ключа
-	amount++;
-	//...
-	checkLoad();
-}
-
-void HashMap::remove(string key) { // Удаление по ключу
-	//...
-}
-
 void HashMap::resize() { // Увеличение размера HashMap
 	int size_ = size; // Запоминаем старый размер
 	LinkedList* arr_ = arr; // Запоминаем текущий массив списков
@@ -57,6 +41,32 @@ void HashMap::checkLoad() { // Проверка загрузки HashMap
 	if (loadFactor > maxLoad) { // Текущий коэффициент заполнения превышает максимальный
 		resize(); // Увеличиваем размер
 	}
+}
+
+int HashMap::hash(string str) { // Нахождение хеша
+	int hash = 0;
+	for (int i = 0; i < 6; i++) {
+		hash += abs(static_cast<int>(str[i]))*(i+1);
+	}
+	return hash;
+}
+
+void HashMap::add(string value) { // Добавление, хеш на основе самого значения
+	amount++; // Инкрементируем общее количество элементов
+	int index = hash(value) % size; // Находим позицию в массиве
+	arr[index].add(value);
+	checkLoad(); // Проверяем коэффициент заполнения HashMap
+}
+
+void HashMap::add(string key, string value) { // Добавление, хеш на основе переданного ключа
+	amount++; // Инкрементируем общее количество элементов
+	int index = hash(key) % size; // Находим позицию в массиве
+	arr[index].add(key, value);
+	checkLoad(); // Проверяем коэффициент заполнения HashMap
+}
+
+void HashMap::remove(string key) { // Удаление по ключу
+	//...
 }
 
 string HashMap::find(string key) { // Поиск по ключу
--- lab2/headers/HashMap.h
+++ lab2/headers/HashMap.h
@@ -9,11 +9,16 @@ using namespace std;
 // HashMap класс
 class HashMap {
 private:
+	// Поля
 	LinkedList* arr; // Массив связных списков 
 	int size; // Размер массива (количество списков)
 	int amount; // Общее количество хранимых значений
 	const static int defaultSize; // Размер по умолчанию
 	const static double maxLoad; // Максимальный допустимый коэффициент заполнения
+	// Методы
+	void resize(); // Увеличение размера HashMap
+	void checkLoad(); // Проверка загрузки HashMap
+	int hash(string key); // Хеш функция
 public:
 	// Конструкторы
 	HashMap(); // По умолчанию
@@ -22,8 +27,6 @@ public:
 	void add(string value); // Добавление, хеш на основе самого значения
 	void add(string key, string value); // Добавление, хеш на основе переданного ключа
 	void remove(string key); // Удаление по ключу
-	void resize(); // Увеличение размера HashMap
-	void checkLoad(); // Проверка загрузки HashMap
 	string find(string key); // Поиск по ключу
 };
 
